79883f82bef3126df890e347b120dec7eb921770
aiguo-zhao <2860729247@qq.com>
2021-05-29 01:41:37 +0800

    Modify FEC_ Main. C file

--- linux-imx-rel_imx_4.1.15_2.1.0_ga/drivers/net/ethernet/freescale/fec_main.c
+++ linux-imx-rel_imx_4.1.15_2.1.0_ga/drivers/net/ethernet/freescale/fec_main.c
@@ -3448,9 +3448,18 @@ fec_probe(struct platform_device *pdev)
 	struct device_node *np = pdev->dev.of_node, *phy_node;
 	int num_tx_qs;
 	int num_rx_qs;
+	/* 设置 MX6UL_PAD_ENET1_TX_CLK 和 MX6UL_PAD_ENET2_TX_CLK
+	 * 这两个 IO 的复用寄存器的 SION 位为 1。
+	 */
+	 void __iomem *IMX6U_ENET1_TX_CLK;
+ 	void __iomem *IMX6U_ENET2_TX_CLK;
+	IMX6U_ENET1_TX_CLK = ioremap(0X020E00DC, 4);
+	writel(0X14, IMX6U_ENET1_TX_CLK);
+	IMX6U_ENET2_TX_CLK = ioremap(0X020E00FC, 4);
+	writel(0X14, IMX6U_ENET2_TX_CLK);
 
 	fec_enet_get_queue_num(pdev, &num_tx_qs, &num_rx_qs);
-
+	
 	/* Init network device */
 	ndev = alloc_etherdev_mqs(sizeof(struct fec_enet_private),
 				  num_tx_qs, num_rx_qs);
