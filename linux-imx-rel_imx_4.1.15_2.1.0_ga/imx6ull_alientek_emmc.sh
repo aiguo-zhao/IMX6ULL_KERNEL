f2f45ffa95f7d3d554f238a2ebcb2db2f931e90c
aiguo-zhao <2860729247@qq.com>
2021-05-29 03:04:37 +0800

    copy zImage dtb to top directory

--- linux-imx-rel_imx_4.1.15_2.1.0_ga/imx6ull_alientek_emmc.sh
+++ linux-imx-rel_imx_4.1.15_2.1.0_ga/imx6ull_alientek_emmc.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 make imx_alientek_emmc_defconfig
 make -j2
-
+cp /home/mk/linux2/alientek_linux/linux-imx-rel_imx_4.1.15_2.1.0_ga/arch/arm/boot/zImage  ./
+cp /home/mk/linux2/alientek_linux/linux-imx-rel_imx_4.1.15_2.1.0_ga/arch/arm/boot/dts/imx6ull-alientek-emmc.dtb ./
